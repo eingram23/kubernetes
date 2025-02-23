9a312b3be1fce20aaff57f9f2c6a38a2f1776415
Edward Ingram <edward.ingram23@gmail.com>
2025-02-23 11:26:46 -0800

    update values

--- create_vault_cert_secret.sh
+++ create_vault_cert_secret.sh
@@ -2,7 +2,7 @@ kubectl create secret generic vault-server-tls \
     --namespace vault \
     --save-config \
     --dry-run=client \
-    --from-file=tls.key=cert.key \
-    --from-file=tls.crt=cert.crt \
-    --from-file=ca.crt=cert.ca \
+    --from-file=tls.key=vault-k3s.key \
+    --from-file=tls.crt=vault-k3s.crt \
+    --from-file=ca.crt=vault.ca \
     -o yaml | kubectl apply -f -
